e16abe708fa785b55fb16f76bb0f2f303259b63d
eugeniecm <97541454+eugeniecm@users.noreply.github.com>
2022-04-30 13:58:13 -0400

    started working on the game

--- game.py
+++ game.py
@@ -1,5 +1,22 @@
 import requests
 
-url = "https://opentdb.com/api.php?amount=50"
+url = "https://opentdb.com/api.php?amount=10"
 
-print()
+#this code fecthes the questions
+question_list = []
+
+for q in response_data["results"]:
+    question_list.append(q["question"])
+print(question_list)
+
+
+#this code fetches the answers
+answer_list = []
+
+for q in response_data["results"]:
+    answer_list.append(q["correct_answer"])
+print(answer_list)
+
+
+for question in question_list  
+    print(question["question"])
