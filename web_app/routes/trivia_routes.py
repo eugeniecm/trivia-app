577ea642685953705479ad88413e603bcb379a17
eugeniecm <97541454+eugeniecm@users.noreply.github.com>
2022-04-30 13:57:30 -0400

    updated html files

---
+++ web_app/routes/trivia_routes.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, request, jsonify, render_template, redirect, flash # FYI new imports
+
+trivia_routes = Blueprint("trivia_routes", __name__)
+
+@trivia_routes.route("/trivia/game.json")
+def trivia_game_api():
+
+    print("THIS IS YOUR TRIVIA GAME (API)...")
+    print("URL PARAMS:", dict(request.args))
+
+    # country_code = request.args["country_code"] #the dict might not have this key all the time
+    country_code = request.args.get("country_code") or "US"
+    zip_code = request.args.get("zip_code") or "20057"
+
+    results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
+    if results:
+        return jsonify(results)
+    else:
+        return jsonify({"message":"Invalid Geography. Please try again."}), 404
+
+@weather_routes.route("/weather/form")
+def weather_form():
+    print("WEATHER FORM...")
+    return render_template("weather_form.html")
+
+@weather_routes.route("/weather/forecast", methods=["GET", "POST"])
+def weather_forecast():
+    print("WEATHER FORECAST...")
+
+    if request.method == "GET":
+        print("URL PARAMS:", dict(request.args))
+        request_data = dict(request.args)
+    elif request.method == "POST": # the form will send a POST
+        print("FORM DATA:", dict(request.form))
+        request_data = dict(request.form)
+
+    country_code = request_data.get("country_code") or "US"
+    zip_code = request_data.get("zip_code") or "20057"
+
+    results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
+    if results:
+        flash("Weather Forecast Generated Successfully!", "success")
+        return render_template("weather_forecast.html", country_code=country_code, zip_code=zip_code, results=results)
+    else:
+        flash("Geography Error. Please try again!", "danger")
+        return redirect("/weather/form")
