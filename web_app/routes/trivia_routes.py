edf7718ddc04369d9926b3de2ef92d89e75f22ec
eugeniecm <97541454+eugeniecm@users.noreply.github.com>
2022-05-01 18:52:44 -0400

    changes in functions

--- web_app/routes/trivia_routes.py
+++ web_app/routes/trivia_routes.py
@@ -4,12 +4,13 @@ from app.trivia_service import list_question
 
 trivia_routes = Blueprint("trivia_routes", __name__)
 
-@trivia_routes.route("/trivia/game.json")
-def trivia_game_api():
-
-    print("THIS IS YOUR TRIVIA GAME (API)...")
-    print("URL PARAMS:", dict(request.args))
-
+#@trivia_routes.route("/trivia/game.json")
+#def trivia_game_api():
+#
+#    print("THIS IS YOUR TRIVIA GAME (API)...")
+#    print("URL PARAMS:", dict(request.args))
+#
+#    return ----
     #accept user input here. adapt to nmbr of players later
 
     # country_code = request.args["country_code"] #the dict might not have this key all the time
@@ -24,29 +25,29 @@ def trivia_game_api():
     #else:
         #return jsonify({"message":"Invalid Geography. Please try again."}), 404
 
-@weather_routes.route("/weather/form")
-def weather_form():
-    print("WEATHER FORM...")
-    return render_template("weather_form.html")
+@trivia_routes.route("/trivia/game", methods=["POST"])
+def trivia_game():
+    print("THIS IS YOUR TRIVIA GAME...")
+    return render_template("trivia_game.html")
 
-@weather_routes.route("/weather/forecast", methods=["GET", "POST"])
-def weather_forecast():
-    print("WEATHER FORECAST...")
-
-    if request.method == "GET":
-        print("URL PARAMS:", dict(request.args))
-        request_data = dict(request.args)
-    elif request.method == "POST": # the form will send a POST
-        print("FORM DATA:", dict(request.form))
-        request_data = dict(request.form)
-
-    country_code = request_data.get("country_code") or "US"
-    zip_code = request_data.get("zip_code") or "20057"
-
-    results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
-    if results:
-        flash("Weather Forecast Generated Successfully!", "success")
-        return render_template("weather_forecast.html", country_code=country_code, zip_code=zip_code, results=results)
-    else:
-        flash("Geography Error. Please try again!", "danger")
-        return redirect("/weather/form")
+#@weather_routes.route("/weather/forecast", methods=["GET", "POST"])
+#def weather_forecast():
+#    print("WEATHER FORECAST...")
+#
+#    if request.method == "GET":
+#        print("URL PARAMS:", dict(request.args))
+#        request_data = dict(request.args)
+#    elif request.method == "POST": # the form will send a POST
+#        print("FORM DATA:", dict(request.form))
+#        request_data = dict(request.form)
+#
+#    country_code = request_data.get("country_code") or "US"
+#    zip_code = request_data.get("zip_code") or "20057"
+#
+#    results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
+#    if results:
+#        flash("Weather Forecast Generated Successfully!", "success")
+#        return render_template("weather_forecast.html", country_code=country_code, zip_code=zip_code, results=results)
+#    else:
+#        flash("Geography Error. Please try again!", "danger")
+#        return redirect("/weather/form")
