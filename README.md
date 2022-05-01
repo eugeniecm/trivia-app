e78e26aeec62b2ad243cb4f17db14265ba511ff9
eugeniecm <97541454+eugeniecm@users.noreply.github.com>
2022-05-01 15:55:16 -0400

    started working on the app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # trivia-app
+
+
+#to activate the repo on the terminal please run 
+```
+conda activate trivia-env
+```
---
+++ app/trivia_service.py
@@ -0,0 +1,33 @@
+# here is the code to actually run the game 
+# this means display the questions and answers on the interface
+
+import requests
+import json
+from pprint import pprint
+import janitor
+
+url = "https://opentdb.com/api.php?amount=10"
+
+pprint(response_data)
+
+#this selects all the questions and appends it to this list
+question_list = []
+
+for q in response_data["results"]:
+    question_list.append(q["question"])
+print(question_list)
+
+#this is supposed to print out all of the questions but it prints out the answers as well
+for question in question_list:  
+    print(question)
+
+#this selects all the answers and appends it to this list
+answer_list = []
+
+for q in response_data["results"]:
+    answer_list.append(q["correct_answer"])
+print(answer_list)
+
+#this is supposed to print out all of the questions but it 
+for answer in answer_list:  
+    print(answer)
